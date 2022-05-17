4c2e2d276ba7f6e07b6edc5842e4e5ad5867ab83
Ramanantsoa Kevin <ramanantsoa.vlm@gmail.com>
2022-05-17 14:00:22 +0200

    avancement du JS

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-let tall = document.getElementById('tall')
-let weight = document.getElementById('weight')
-let bodyfat = document.getElementById('body-fat')
+function calculMass() {
+     let tall = document.getElementById('tall')
+     let weight = document.getElementById('weight')
+     let bodyfat = document.getElementById('body-fat')
 
+     function leanWeight() {
+          return parseInt(weight.value) * (1 - (parseInt(bodyfat.value) / 100))
+     }
 
-function leanWeight() {
-     return parseInt(tall.value) * (1 - (parseInt(bodyfat.value)/100))
+     let lean = leanWeight()
+
+     function ffmi() {
+          return lean / (parseInt(tall.value * tall.value))
+     }
+
+     let result = ffmi()
+     
+     let button = document.getElementById('btn-calcul')
+     
+     function evenement() {
+          calculMass()
+     }
+     
+     button.addEventListener('click', evenement)
+     document.getElementById('result-total').innerText = result + ' de FFMI'
 }
 
-let lean = leanWeight()
+
+
+
+
